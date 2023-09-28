7754c3f4a6919ce489a69c5e069e17e0ea6aea87
jasont <jason101real@gmail.com>
2023-09-28 10:40:11 -0500

    Runner added

---
+++ src/week1_JavaCodingTasks/Jason/Runner.java
@@ -0,0 +1,11 @@
+package week1_JavaCodingTasks.Jason;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        OddOrEven.OddEven(2);
+        DivideWithoutDivide.divide(6,2);
+
+
+    }
+}
