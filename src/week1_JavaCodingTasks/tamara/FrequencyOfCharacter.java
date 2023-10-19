10502a72e03c66a6520b16ccf8577c31a68db2c1
Tamara29071988 <114584763+Tamara29071988@users.noreply.github.com>
2023-10-19 11:49:24 -0500

    Update FrequencyOfCharacter.java

--- src/week1_JavaCodingTasks/tamara/FrequencyOfCharacter.java
+++ src/week1_JavaCodingTasks/tamara/FrequencyOfCharacter.java
@@ -4,19 +4,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class FrequencyOfCharacter {
-
-      /*  Frequency of Character
-
-        Create a method that will determine the frequency of every character in the given String
-        parameter: String
-        return: void
-
-        Ex:
-        input:
-        frequencyOfCharacter("banana apple pie")
-        output:
-        {b=1, a=4, n=2,  =2, p=3, l=1, e=2, i=1}
-     */
+/*String
+Frequency of Characters
+Write a return method that can find the frequency of
+characters
+Ex:
+FrequencyOfChars ("AAABBCDD") ==> A3B2C1D2*/
+      
 
         public static void main(String[] args) {
             frequencyOfCharacter("AAABBCDD");
